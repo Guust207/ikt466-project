e7325991535bc731477de8d689dc110c2e01d7ee
Guust207 <92439850+Guust207@users.noreply.github.com>
2025-11-24 16:59:41 +0100

    Moved preprocessing to py file

---
+++ preprocessing/wav_to_pyplot.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import os
+from pathlib import Path
+import numpy as np
+import csv
+import re
+
+def get_wav_filenames(path_to_dir: Path):
+    filenames = [file.name for file in path_to_dir.iterdir() if file.is_file()]
+    return filenames
+
+
+def wav_to_png(wav_filename: str):
+    return wav_filename.replace(".wav", ".png")
+
+
+# Paths to directories
+path_to_data = Path.cwd() / ".." / "data"
+path_to_audio = path_to_data / "audio"
+path_to_pyplot = path_to_data / "pyplot"
+
+wav_filenames = get_wav_filenames(path_to_audio)
+
+spectrograms = [] # [y1, sr1], [y2, sr2], ...]
+max_amp = 0
+min_amp = 0
+
+# Loop through all wav files and use librosa for spectrograms
+for idx, audio_filename in enumerate(wav_filenames):
+    audio_file = Path(path_to_audio / audio_filename)
+    spectrogram = librosa.load(audio_file)
+    spectrograms.append(spectrogram)
+
+    max_y = max(spectrogram[0])
+    min_y = min(spectrogram[0])
+
+    max_amp = max_y if max_y > max_amp else max_amp
+    min_amp = min_y if min_y < min_amp else min_amp
+
+# Loop through all spectrograms and plot them
+for idx, spectrogram in enumerate(spectrograms):
+    # Plot
+    plt.plot(spectrogram[0])
+    plt.ylim([min_amp, max_amp])
+    plt.axis("off")
+    plt.draw()
+
+    # Proper filename
+    png_filename = wav_to_png(wav_filenames[idx])
+    file_to_save = path_to_pyplot / png_filename
+
+    # Save file
+    plt.savefig(file_to_save)
